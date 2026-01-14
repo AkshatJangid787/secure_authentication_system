ca276d9b43013ae585acdcc1e5528660089b27ef
akshatjangid787 <akshatjangid14@gmail.com>
2026-01-14 02:13:16 -0600

    Login and with 2FA

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import {ToastContainer} from "react-toastify"
+import VerifyOtp from './pages/VerifyOtp'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/verifyotp' element={<VerifyOtp/>}/>
+        </Routes>
+        <ToastContainer/>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { server } from '../main'
+import api from '../apiIntercepter';
+
+const AppContext = createContext(null)
+
+export const AppProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [isAuth, setIsAuth] = useState(false)
+
+    async function fetchUser() {
+        setLoading(true)
+        try {
+            const { data } = await api.get(`/api/v1/me`);
+
+            setUser(data)
+            setIsAuth(true)
+
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+    return (
+        <AppContext.Provider value={{
+            setIsAuth, isAuth, user, setUser, loading
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+};
+
+export const AppData = () => {
+    const context = useContext(AppContext)
+
+    if (!context) throw new Error("AppData must be used within an AppProvider");
+
+    return context;
+}
