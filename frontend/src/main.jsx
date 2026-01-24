e96df4aeb7ac02717fab1d0917de2ca7a253ce72
akshatjangid787 <akshatjangid14@gmail.com>
2026-01-24 16:18:50 +0530

    fix server api

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { AppProvider } from './context/AppContext.jsx';
 
-export const server = import.meta.env.VITE_API_URL || "http://localhost:3000";
+export const server = import.meta.env.VITE_API_URL;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
